47cbd120877a7737483489a741d3020d5b4c020d
Mira <mira.ph.lin@gmail.com>
2019-04-22 12:02:31 +0200

    lab three

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  cattr_accessor :current_user
+  has_many :articles
+  has_many :comments
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ test/models/article_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+#   test 'shouldnot save article with no title' do
+#     article = Article.new
+#     assert_not article.save
+# end
+end
